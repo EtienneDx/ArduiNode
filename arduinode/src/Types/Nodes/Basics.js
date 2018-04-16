b6d0852916be6f670195e316767c301ea25b1d8d
EtienneDx <32039535+EtienneDx@users.noreply.github.com>
2018-04-16 14:23:12 +0200

    Added loops

--- arduinode/src/Types/Nodes/Basics.js
+++ arduinode/src/Types/Nodes/Basics.js
@@ -355,4 +355,116 @@ else {
 	globalVars : [
 	]
 },
+{
+	name : "For Loop",
+	inputs : [
+		{
+			type : "Execution",
+			name : "exec"
+		},
+		{
+			type : "Int",
+			name : "from"
+		},
+		{
+			type : "Int",
+			name : "until"
+		},],
+	outputs : [
+		{
+			type : "Execution",
+			name : "loop"
+		},
+		{
+			type : "Execution",
+			name : "then"
+		},],
+	needsExecution : true,
+	becomes : `for(int i = <<inputs:from>>; i < <<inputs:until>>; i++)
+{
+    <outputs:loop>>
+}
+<<outputs:then>>`,
+	globalVars : [],
+},
+{
+	name : "While Loop",
+	inputs : [
+		{
+			type : "Execution",
+			name : "exec"
+		},
+		{
+			type : "Boolean",
+			name : "condition"
+		},],
+	outputs : [
+		{
+			type : "Execution",
+			name : "loop"
+		},
+		{
+			type : "Execution",
+			name : "then"
+		},],
+	needsExecution : true,
+	becomes : `while(<<inputs:condition>>)
+{
+    <outputs:loop>>
+}
+<<outputs:then>>`,
+	globalVars : [],
+},
+{
+	name : "Do While Loop",
+	inputs : [
+		{
+			type : "Execution",
+			name : "exec"
+		},
+		{
+			type : "Boolean",
+			name : "condition"
+		},],
+	outputs : [
+		{
+			type : "Execution",
+			name : "loop"
+		},
+		{
+			type : "Execution",
+			name : "then"
+		},],
+	needsExecution : true,
+	becomes : `do
+{
+    <outputs:loop>>
+} while(<<inputs:condition>>);
+<<outputs:then>>`,
+	globalVars : [],
+},
+{
+	name : "Break Loop",
+	inputs : [
+		{
+			type : "Execution",
+			name : "exec"
+		},],
+	outputs : [],
+	needsExecution : true,
+	becomes : `break;`,
+	globalVars : [],
+},
+{
+	name : "Continue Loop",
+	inputs : [
+		{
+			type : "Execution",
+			name : "exec"
+		},],
+	outputs : [],
+	needsExecution : true,
+	becomes : `continue;`,
+	globalVars : [],
+},
 ]
